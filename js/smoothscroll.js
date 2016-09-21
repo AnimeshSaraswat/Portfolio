d2014b5ee16470c7b1071a57b36780f7beaedb3c
AnimeshSaraswat <animeshsaraswat32517@gmail.com>
2016-09-22 05:11:08 +0530

    landing page complete except the footer part, starting of the work description page done, need to add screen shots and describe about the projects.

---
+++ js/smoothscroll.js
@@ -0,0 +1,28 @@
+window.smoothScroll = (function() {
+  var timer, start, factor;
+
+  return function(target, duration) {
+    var offset = window.pageYOffset,
+    delta = target - window.pageYOffset;  // Y-offset difference
+    duration = duration || 1000;          // default 2s animation
+    start = Date.now();                   // getting the start time
+    factor = 0;
+
+    if (timer) {
+      clearInterval(timer); // stop any running animation
+    }
+
+    function step() {
+      var y;
+      factor = (Date.now() - start) / duration;
+      if (factor >= 1) {
+        clearInterval(timer);
+        factor = 1;
+      }
+      y = factor * delta + offset;
+      window.scrollBy(0, y - window.pageYOffset);
+    }
+    timer = setInterval(step, 10);
+    return timer;
+  }
+})();
